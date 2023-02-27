cafaa77d356b72462545872b2a42f8e618b48913
Edward Baker <eddean@email.unc.edu>
2023-02-27 15:16:06 -0500

    finished rps

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -17,4 +17,14 @@ if (args.r || args.rules) {
     process.exit(0);
 }
 
-console.log(rps(args._[0]));
+// Pass arguments to rps function
+let res = rps(args._[0]);
+if (res == 'out-of-range error'){
+    console.error(args._[0] + ' is out of range.\n');
+    console.log('Usage: node-rps [SHOT] \nPlay Rock Paper Scissors (RPS) \n \n  -h, --help      display this help message and exit \n  -r, --rules     display the rules and exit \n\nExamples: \n  node-rps        Return JSON with single player RPS result. \n                  e.g. {"player":"rock"} \n  node-rps rock   Return JSON with results for RPS played against a simulated opponent. \n                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
+    console.log('\n');
+    console.log('Rules for Rock Paper Scissors:\n\n  - Scissors CUTS Paper \n  - Paper COVERS Rock\n  - Rock CRUSHES Scissors');
+}
+else{
+    console.log(JSON.stringify(res));
+}
