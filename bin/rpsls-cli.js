b121a10f124a3506d6fd485ccf631b66f7873b6b
Edward Baker <eddean@email.unc.edu>
2023-02-27 15:50:11 -0500

    fixing out of range output

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -20,7 +20,6 @@ if (args.r || args.rules) {
 // Pass arguments to rps function
 let res = rpsls(args._[0]);
 if (res == 'out-of-range error'){
-    console.error(args._[0] + ' is out of range.\n');
     console.log('Usage: node-rpsls [SHOT] \nPlay the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)! \n \n  -h, --help      display this help message and exit \n  -r, --rules     display the rules and exit \n\nExamples: \n  node-rpsls       Return JSON with single player RPS result. \n                   e.g. {"player":"rock"} \n  node-rpsls rock  Return JSON with results for RPS played against a simulated opponent. \n                   e.g {"player":"rock","opponent":"Spock","result":"lose"}');
     console.log('\n');
     console.log('Rules for the Lizard-Spock Espansion of Rock Paper Scissors:\n\n  - Scissors CUTS Paper \n  - Paper COVERS Rock\n  - Rock SMOOSHES Lizard\n  - Lizard POISONS Spock\n  - Spock SMASHES Scissors\n  - Scissors DECAPITATES Lizard\n  - Lizard EATS Paper\n  - Paper DISPROVES Spock\n  - Spock VAPORIZES Rock\n  - Rock CRUSHES Scissors');
